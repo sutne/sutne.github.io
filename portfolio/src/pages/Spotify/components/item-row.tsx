5bf3f3370a7b0a5796c97dc70a2995b38310b394
sutne <sivertutne@gmail.com>
2023-04-05 01:49:54 +0200

    UI tweaking

--- portfolio/src/pages/Spotify/components/item-row.tsx
+++ portfolio/src/pages/Spotify/components/item-row.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Stack, useTheme } from '@mui/material';
+import { Box, Stack } from '@mui/material';
+
+import { useTheme } from 'providers/theme-provider';
 
 
 
@@ -7,33 +9,39 @@ type props = {
   children: JSX.Element[];
 }
 export function ItemRow({ ...props }: props): JSX.Element {
-  const theme = useTheme();
+  const { theme } = useTheme();
 
   const style = {
     overflowX: "auto",
     paddingY: "0.5em",
-    [theme.breakpoints.down('sm')]: {
-      '&::-webkit-scrollbar': {
-        height: "0.4em",
-        bgcolor: "rgba(0,0,0,0)",
-      },
-      '&::-webkit-scrollbar-thumb': {
-        bgcolor: "background.paper",
-        borderRadius: "0.4em",
-      }
+    '&::-webkit-scrollbar': {
+      height: { xs: "0.4em", sm: "0.8em" },
+      bgcolor: "rgba(0,0,0,0)",
     },
-    [theme.breakpoints.up('sm')]: {
-      '&::-webkit-scrollbar': {
-        height: "0.8em",
-        bgcolor: "rgba(0,0,0,0)",
-      },
-      '&::-webkit-scrollbar-thumb': {
-        bgcolor: "background.paper",
-        borderRadius: "0.8em",
-      }
+    '&::-webkit-scrollbar-thumb': {
+      bgcolor: "background.paper",
+      borderRadius: { xs: "0.4em", sm: "0.8em" },
     },
-  }
-  return <Stack direction="row" spacing={3} sx={style}>
-    {props.children}
-  </Stack>
+  };
+  const container = {
+    position: "relative",
+    marginRight: "-4mm",
+    '&:after': {
+      content: '""',
+      position: "absolute",
+      top: 0,
+      right: 0,
+      zIndex: 1,
+      pointerEvents: "none",
+      width: "100%",
+      height: "95%",
+      backgroundImage: `linear-gradient(to right, ${theme.palette.background.default}, rgba(0, 0, 0, 0) 1% 98%, ${theme.palette.background.default})`,
+    },
+  };
+  return <Box sx={container}>
+    <Stack direction="row" spacing={3} sx={style}>
+      {props.children}
+    </Stack>
+  </Box>;
+
 }
--- portfolio/src/pages/Spotify/sections/NowPlaying.tsx
+++ portfolio/src/pages/Spotify/sections/NowPlaying.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { VolumeMute, VolumeUp } from '@mui/icons-material';
-import { Box, IconButton, Stack, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { VolumeOff, VolumeUp } from '@mui/icons-material';
+import { Box, collapseClasses, Grow, IconButton, Stack, Typography } from '@mui/material';
 
-import { ProgressBar } from '../components/progressbar';
+import { TimeDuration } from '../components/progressbar';
 import { SectionTitle } from '../components/typography';
 import { useMusicPlayer } from '../providers/music-player';
 import * as API from '../service/api';
@@ -10,92 +10,138 @@ import { TypeNowPlaying } from '../service/types';
 
 
 export function NowPlaying(): JSX.Element {
-  const [track, setTrack] = React.useState<TypeNowPlaying | undefined>();
+  // State
+  const [track, setTrack] = useState<TypeNowPlaying | undefined>();
+  const [isHovering, setIsHovering] = useState(false);
+
+  // Providers
   const { addSample, handlePlayPause, isPlaying } = useMusicPlayer();
 
+  // Effects
   useEffect(() => {
     if (track) return;
     const getTrack = async () => {
       const response = await API.getNowPlaying();
       if (!response) return;
       setTrack(response);
-      addSample(response.sample)
-    }
+      addSample(response.sample);
+    };
     getTrack();
-  }, [track])
+  }, [track]);
 
-  const classes = {
-    container: [{
-      position: "relative",
-      borderRadius: "8px",
-      overflow: "hidden",
-      padding: "16px",
-      bgcolor: "rgb(40,40,40)",
-    }],
-    cover: [{
-      width: "min(50mm, 100%)",
-      height: "min(50mm, 100%)",
-      borderRadius: "8px",
-    }],
-    content: [{
-      position: "relative",
-      marginLeft: "16px",
-      width: "100%",
-      height: "fill-parent",
-      textAlign: "center",
-    }],
-    info: [{
-      width: "100%",
-      height: "100%",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    }],
-    title: [{
-      color: "rgba(255,255,255,0.9)",
-      fontSize: "2.2em",
-      whiteSpace: "nowrap",
-      textOverflow: "ellipsis",
-    }],
-    artists: [{
-      color: "rgba(255,255,255,0.5)",
-      fontSize: "1.4em",
-      lineHeight: "1.6em",
-      height: "3.2em",
-      overflow: "hidden",
-      textOverflow: "ellipsis",
-    }],
-    progress: [{
-      width: "100%",
-      position: "absolute",
-      bottom: "0",
-      left: "0",
-    }],
+  // Helpers
+  const showButton = () => isHovering || (track && isPlaying(track.sample));
+
+  // Structure
+  const classes = getClasses();
+  return <Grow in={false} timeout={400}>
+    {track ?
+      <>
+        <SectionTitle title="Currently Listening To" />
+        <Stack direction={{ xs: "column", sm: "row" }} alignContent="stretch" sx={classes.container}
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
+        >
+          <Box sx={classes.cover_box}>
+            <Box component="img" src={track.art} sx={classes.cover} />
+            <IconButton sx={classes.button} onClick={() => handlePlayPause(track.sample)}>
+              {isPlaying(track.sample) ? <VolumeUp sx={classes.buttonIcon} /> : <VolumeOff sx={classes.buttonIcon} />}
+            </IconButton>
+          </Box>
+          <Stack direction="column" sx={classes.content}>
+            <Box sx={classes.info}>
+              <Typography sx={classes.title}>{track.title}</Typography>
+              <Typography sx={classes.artists}>{track.artists.join(", ")}</Typography>
+              <Box sx={classes.progress}>
+                <TimeDuration
+                  length={track.length}
+                  progress={track.progress}
+                  onCompletion={() => { setTrack(undefined); }}
+                />
+              </Box>
+            </Box>
+          </Stack>
+        </Stack >
+      </> : <></>
+    }
+  </Grow>;
+
+  // Style
+  function getClasses() {
+    return {
+      container: [{
+        borderRadius: "8px",
+        padding: "16px",
+        textAlign: "center",
+        bgcolor: "background.paper",
+      }],
+      cover_box: [{
+        position: "relative",
+      }],
+      cover: [{
+        width: "50mm",
+        borderRadius: "8px",
+        aspectRatio: "1",
+        objectFit: "cover",
+      }],
+      button: [{
+        position: "absolute",
+        right: "5mm",
+        bottom: "5mm",
+        height: "52px",
+        width: "52px",
+        transition: "all 0.3s ease-in-out",
+        bgcolor: "primary.main",
+        color: "background.paper",
+        opacity: showButton() ? "1" : "0",
+        transform: showButton() ? "translate(0,0)" : "translate(0,3mm)",
+        "&:hover": {
+          bgcolor: "primary.light",
+        },
+      }],
+      buttonIcon: {
+        color: "rgb(24,24,24)",
+        transition: "all 0.125s ease",
+        fontSize: "1.3em",
+      },
+      content: [{
+        position: "relative",
+        width: "100%",
+        padding: { xs: "0 0 0 0", sm: "0 0 0 16px" },
+        display: "table",
+      }],
+      info: [{
+        display: "table-cell",
+        maxWidth: "0px",
+        verticalAlign: "middle",
+        textAlign: "center",
+        position: "relative",
+        overflow: "hidden",
+        height: { xs: "6em", sm: "100%" },
+      }],
+      title: [{
+        color: "text.primary",
+        width: "100%",
+        fontSize: { xs: "1.2em", sm: "1.8em" },
+        display: "block",
+        overflow: "hidden",
+        whiteSpace: "nowrap",
+        textOverflow: "ellipsis",
+      }],
+      artists: [{
+        color: "text.secondary",
+        fontSize: { xs: "0.9em", sm: "1.2em" },
+        lineHeight: { xs: "0.9em", sm: "1.2em" },
+        height: { xs: "1.8em", sm: "2.4em" },
+        overflow: "hidden",
+        margin: "0 30px",
+      }],
+      progress: [{
+        position: "absolute",
+        width: "100%",
+        bottom: "0",
+        left: "0",
+      }],
+    };
   }
-
-
-  if (!track) return <></>
-  return <>
-    <SectionTitle title="Currently Listening To" />
-    <Stack direction={{ xs: "column", sm: "row" }} sx={classes.container}>
-      <Box component="img" src={track.art} sx={classes.cover} />
-      <Stack direction="column" sx={classes.content}>
-        <Box sx={classes.info}>
-          <Typography sx={classes.title}>{track.title}</Typography>
-          <Typography sx={classes.artists}>{track.artists.join(", ")}</Typography>
-        </Box>
-        <Stack sx={classes.progress} direction="row" spacing={1}>
-          <ProgressBar
-            length={track.length}
-            progress={track.progress}
-            onCompletion={() => { setTrack(undefined) }} />
-          <IconButton onClick={() => handlePlayPause(track.sample)}>
-            {isPlaying(track.sample) ? <VolumeUp /> : <VolumeMute />}
-          </IconButton>
-        </Stack>
-      </Stack>
-    </Stack >
-  </>
-
 }
