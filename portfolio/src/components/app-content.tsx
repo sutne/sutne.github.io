20a1fba9c6e9994146ecbe411d325b894a2ba254
sutne <sivertutne@gmail.com>
2023-04-06 18:16:23 +0200

    fix scrolling and scrollbar

--- portfolio/src/components/app-content.tsx
+++ portfolio/src/components/app-content.tsx
@@ -12,102 +12,131 @@ type props = {
 export function AppContent({ children }: props) {
 
   const { theme } = useTheme();
-  const { name, close, isOpen } = useApp();
-
-  const animationDuration = isOpen ? 750 : 350;
+  const { name, close } = useApp();
 
   const sx = getSx();
   return (
-    <>
-      <Fade in={isOpen} timeout={animationDuration}>
-        <Box sx={sx.open_background} onClick={() => close()} />
-      </Fade>
-      <Zoom in={isOpen} timeout={animationDuration}>
-        <Box sx={sx.content_wrapper}>
-          <Box sx={sx.content}>
-            <Stack direction='row' sx={sx.title}>
-              <Box sx={sx.title_icon} component='img'
-                src={require(`assets/apps/${name}.png`)}
-              />
-              <Typography alignSelf='center' sx={sx.app_name}>{name}</Typography>
-              <IconButton sx={sx.close_button} onClick={() => close()}><Minimize /></IconButton>
-            </Stack>
-            <Box sx={sx.app_content}>
-              {children}
-            </Box>
-          </Box>
+    <AppContentWrapper>
+      <Box sx={sx.content}>
+        <Stack direction='row' sx={sx.title}>
+          <Box sx={sx.title_icon} component='img'
+            src={require(`assets/apps/${name}.png`)}
+          />
+          <Typography sx={sx.app_name} alignSelf='center'>{name}</Typography>
+          <IconButton sx={sx.close_button} onClick={() => close()}><Minimize /></IconButton>
+        </Stack>
+        <Box sx={sx.app_content}>
+          {children}
         </Box>
-      </Zoom>
-    </>
+      </Box>
+    </AppContentWrapper>
   );
 
   function getSx() {
     return {
-      close_button: [{
-        alignSelf: 'center',
-        height: '2em',
-        width: '2em',
-      }],
-      open_background: [
-        {
-          zIndex: '100',
-          position: 'fixed',
-          top: '0',
-          left: '0',
-          height: '100%',
-          width: '100%',
-          bgcolor: 'rgba(0, 0, 0, 25%)',
-        },
-      ],
-      content_wrapper: [{
-        zIndex: '150',
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        right: '0',
-        bottom: '0',
-        width: 'min(1024px, 100%)',
+      content: {
+        width: { xs: '100%', sm: 'fit-content' },
         margin: 'auto',
-        paddingY: '2em',
-        overflow: 'auto',
-        '&::-webkit-scrollbar': {
-          width: '0',
-        },
-      }],
-      content: [{
-        zIndex: '200',
-        position: 'relative',
-        borderRadius: '16px',
+        maxWidth: '100%',
         height: 'fit-content',
-        maxWidth: '90%',
-        margin: '3mm auto',
+        borderRadius: '16px',
         overflow: 'hidden',
         bgcolor: theme.palette.background.default,
         color: theme.palette.text.primary,
-        boxShadow: '0px 12px 24px rgba(0, 0, 0, 50%)',
-      }],
-      title: [{
+        boxShadow: '0px 4px 12px rgba(0, 0, 0, 70%)',
+      },
+      title: {
         padding: '12px',
         background: `linear-gradient(0deg, 
           ${alpha(theme.palette.background.paper, 0.4)} 0%,
           ${alpha(theme.palette.background.paper, 1)} 100%
         )`,
-      }],
-      title_icon: [{
+        boxShadow: '0px 3px 3px rgba(0,0,0,40%)',
+      },
+      title_icon: {
         width: '3em',
         height: '3em',
         borderRadius: '25%',
-      }],
-      app_name: [{
-        paddingLeft: '16px',
+      },
+      app_name: {
+        paddingX: '16px',
         fontSize: '1.5em',
         lineHeight: '100%',
         width: '100%',
-      }],
-      app_content: [{
+      },
+      close_button: {
+        alignSelf: 'center',
+        height: '2em',
+        width: '2em',
+        color: theme.palette.text.primary,
+      },
+      app_content: {
         padding: '24px',
-        boxShadow: 'inset 0px 3px 3px rgba(0,0,0,40%)',
-      }],
+      },
+    };
+  }
+}
+
+
+
+
+type wrapper_props = {
+  children: JSX.Element
+}
+export function AppContentWrapper({ children }: wrapper_props) {
+
+  const { close, isOpen } = useApp();
+  const animationDuration = isOpen ? 750 : 350;
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    if (e.target === e.currentTarget) {
+      close();
+    }
+  };
+
+  const sx = getSx();
+  return (
+    <>
+      <Fade in={isOpen} timeout={animationDuration}>
+        <Box sx={sx.background} onClick={handleClick} >
+          <Box sx={sx.reset} onClick={handleClick}>
+            <Box sx={sx.container} onClick={handleClick}>
+              <Zoom in={isOpen} timeout={animationDuration}>
+                {children}
+              </Zoom>
+            </Box>
+          </Box>
+        </Box>
+      </Fade>
+    </>
+  );
+
+  function getSx() {
+    return {
+      background: {
+        zIndex: 1,
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,70%)',
+        overflowY: 'auto',
+        '&::-webkit-scrollbar': {
+          width: '0em',
+        },
+      },
+      reset: {
+        zIndex: 1,
+        position: 'absolute',
+        width: 'min(1024px, 100%)',
+      },
+      container: {
+        margin: '16px',
+      },
     };
   }
 }
--- portfolio/src/components/app-drawer.tsx
+++ portfolio/src/components/app-drawer.tsx
@@ -36,21 +36,16 @@ export function AppDrawer({ ...props }: props) {
         borderRadius: '16px',
         transition: 'all ease-in-out 0.125s',
         boxShadow: '3px 3px 10px 1pt rgba(0,0,0, 30%)',
-        // transform: "scale(1)",
-        '&:hover': {
-          // transform: "scale(1.01)",
-          boxShadow: '5px 5px 12px 2pt rgba(0,0,0, 20%)',
-        },
       }],
       drawer: [{
         padding: '24px',
-        boxShadow: 'inset 1px 2px 4px rgba(0, 0, 0, 20%)',
       }],
       title: [{
         fontWeight: 'bold',
         color: 'text.primary',
         padding: '12px 16px',
         backgroundImage: `linear-gradient(70deg,  ${alpha(theme.palette.background.paper, 0.75)} 60%, ${alpha(theme.palette.background.paper, 0.95)} 100%)`,
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 20%)',
       }],
     };
   }
