11ed108d3993dfa5c7f2cc1de4ccda58d68b56df
sutne <sivert.utne@gmail.com>
2025-03-01 15:55:31 +0100

    fix(spotify): separate shimmer and actual item cards
    as some artists has empty genres it would show as a shimmer/loading instead of correctly just showing an empty list.

--- src/pages/Spotify/components/item-subtitle.tsx
+++ src/pages/Spotify/components/item-subtitle.tsx
@@ -1,14 +1,7 @@
 import { Typography } from '@mui/material';
-import { ShimmerText } from 'components/animated/shimmer';
 
 export function ItemSubtitle(props: { children?: string; sx?: any }) {
   const sx = getSx();
-  if (!props.children)
-    return (
-      <>
-        <ShimmerText fontSize='0.8em' numLines={2} />
-      </>
-    );
   return <Typography sx={sx}>{props.children}</Typography>;
 
   function getSx() {
--- src/pages/Spotify/components/item-title.tsx
+++ src/pages/Spotify/components/item-title.tsx
@@ -1,19 +1,8 @@
 import { Typography } from '@mui/material';
-import { ShimmerText } from 'components/animated/shimmer';
 
 export function ItemTitle(props: { children?: string; sx?: any }) {
   const sx = getSx();
-  if (!props.children)
-    return (
-      <>
-        <ShimmerText fontSize='1.1em' numLines={1} />
-      </>
-    );
-  return (
-    <>
-      <Typography sx={sx}>{props.children}</Typography>
-    </>
-  );
+  return <Typography sx={sx}>{props.children}</Typography>;
 
   function getSx() {
     return [
