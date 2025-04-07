297fc68f7586488e5235e9feb2346f468589609f
sutne <sivert.utne@gmail.com>
2025-04-08 00:12:55 +0200

    feat: add game completion stats

--- src/pages/Playstation/pages/Main/AllTrophies/Stats/TrophyStats.tsx
+++ src/pages/Playstation/pages/Main/AllTrophies/Stats/TrophyStats.tsx
@@ -6,6 +6,7 @@ import {
   EarnedTimePlot,
   EarnedTimePlotShimmer,
 } from './EarnedTimePlot/EarnedTimePlot';
+import { TrophyGameCompletion } from './TrophyGameCompletionRate/TrophyGameCompletionRate';
 
 export function PlaystationTrophyStats() {
   const { isLoading, earnedTimestamps } = useTrophyStats();
@@ -16,7 +17,7 @@ export function PlaystationTrophyStats() {
       <TabBar />
       <Stack spacing={2}>
         <Stack direction='row' spacing='8px'>
-          <Typography sx={sx.header}>Stats for all </Typography>
+          <Typography sx={sx.header}>Stats for </Typography>
           {isLoading ? (
             <ShimmerText fontSize={sx.header.fontSize} width='64px' inline />
           ) : (
@@ -24,20 +25,21 @@ export function PlaystationTrophyStats() {
               {earnedTimestamps?.length}
             </Typography>
           )}
-          <Typography sx={sx.header}> earned trophies.</Typography>
+          <Typography sx={sx.header}> earned trophies</Typography>
         </Stack>
         {isLoading ? (
           <EarnedTimePlotShimmer />
         ) : (
           <EarnedTimePlot timestamps={earnedTimestamps ?? []} />
         )}
+        <TrophyGameCompletion />
       </Stack>
     </>
   );
   function getSx() {
     return {
       header: {
-        fontSize: { xs: '24px', sm: '36px' },
+        fontSize: { xs: '20px', sm: '36px' },
         fontWeight: 200,
       },
     };
